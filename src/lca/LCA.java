bddd98fe8615feefc33a4a9a74b590ffbd72c217
Tom Moran <tomoran@tcd.ie>
2019-09-17 20:10:18 +0100

    Initial commit

---
+++ src/lca/LCA.java
@@ -0,0 +1,16 @@
+package lca;
+
+/**
+ *
+ * @author bobto
+ */
+public class LCA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
